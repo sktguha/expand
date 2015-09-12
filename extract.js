07cf1161d0f28cd19033984b10ff24c1612a5a4c
sktguha <sktguha@gmail.com>
2015-09-13 03:54:22 +0530

    added more acronyms to the list.js and added the extract.js script to get the acronyms
    -one issue is that even pressing backspace triggers the replacement
    script which should not ideally happen as person may be deleting

---
+++ extract.js
@@ -0,0 +1,11 @@
+//this is the extract . extracted from http://www.netlingo.com/acronyms.phps 
+//run this script in chrome devtools after opening the above link
+var elem = document.querySelector('#container > div.body > div.list_box3 > ul');
+var list = {};
+var chs = elem.getElementsByTagName("LI");
+for(var i = 0 ;i< chs.length;i++){
+    var ch = chs[i];
+    var val = ch.childNodes[1].textContent;
+    var key = ch.getElementsByTagName('a')[0].innerText;
+    list[key] = val;
+}
